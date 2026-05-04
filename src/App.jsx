1fa2159ebd46b72121d4816831d7a238d1d377dd
Rajesh Rao <nontechcoding@gmail.com>
2026-05-04 20:41:38 +0530

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@
 import './App.css'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
+import { Routes, Route } from "react-router-dom";
+import Login from './pages/Login';
+import Cart from './pages/Cart';
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -9,8 +12,13 @@ function App() {
   return (
     <>
       <Navbar />
-      <Home />
-      <h1 className='text-3xl font-bold underline'>Hello this uberEats</h1>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/Login' element={<Login />}/>
+        <Route path='/Cart' element={<Cart/>}/>
+
+
+      </Routes>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,19 +4,20 @@ import { BiSolidOffer } from "react-icons/bi";
 import { GiHelp } from "react-icons/gi";
 import { FaUser } from "react-icons/fa";
 import { FaCartShopping } from "react-icons/fa6";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
         <div>
             <div className="w-full h-20 items-center flex">
-                <h1 className="w-1/3 ">Logo</h1>
+                <Link to='/' className="w-1/3 ">Logo</Link>
                 <ul className="w-2/3  flex items-center justify-start text-xl text-gray-700 font-bold gap-16">
                     <li className="flex items-center gap-2"><LiaSuitcaseSolid />Uber Corporate</li>
                     <li className="flex items-center gap-2"><CiSearch />Search</li>
                     <li className="flex items-center gap-2"><BiSolidOffer />Offers</li>
                     <li className="flex items-center gap-2"><GiHelp />Help</li>
-                    <li className="flex items-center gap-2"><FaUser />Sign In</li>
-                    <li className="flex items-center gap-2"><FaCartShopping />Cart</li>
+                    <Link to='/Login'className="flex items-center gap-2"><FaUser />Sign In</Link>
+                    <Link to='/Cart' className="flex items-center gap-2"><FaCartShopping />Cart</Link>
                     {/* <li>Sign Up</li> */}
                 </ul>
             </div>
